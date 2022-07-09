17d6aab2c1287eeeee1c9437f291a900b2770f87
Wu3a <73800639+Froggy-G@users.noreply.github.com>
2022-07-09 18:02:40 +0400

    database
    - creating database and/or connecting when application is startup
    - small changes in dialogues

--- bot.py
+++ bot.py
@@ -1,23 +1,36 @@
 import logging
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, executor
 from config import API_TOKEN
 from commands import get_exchange_rates
+from db import DBHelper
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
+# Initialize bot, dispatcher and database
+db = DBHelper()
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+disp = Dispatcher(bot)
 
 # message handler/sendler
-@dp.message_handler(commands=['start', 'help'])
+@disp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     await message.reply("Привет, я Exchange_rates бот!\nСлежу за текущим курсом криптовалют.")
 
-@dp.message_handler(commands=['value'])
+@disp.message_handler(commands=['myvalue'])
 async def send_current_exchange_rates(message: types.Message):
-    result = get_exchange_rates()
-    # print(result)
-    await message.reply("Готово")
+    if db.get_items(message.from_user.id):
+        await message.answer(db.get_items(message.from_user.id))
+    else:
+        await message.answer("Вы не добавили валюту для слежки")
+
+@disp.message_handler(commands=['addvalue'])
+async def add_cryptocurrency(message: types.Message):
+    await message.answer("Введите название криптовалюты, например BTC")
+    
+
+
+if __name__ == '__main__':
+    db.setup()
+    executor.start_polling(disp, skip_updates=True)
